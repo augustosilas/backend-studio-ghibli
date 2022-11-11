1fa625a6fb0f0595409efdec1c56f588882a1075
Silas Augusto <augusto.fernandes775@gmail.com>
2022-11-11 18:05:09 -0300

    test: ensure FindFilmsUsecase return all films on success
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- src/domain/usecases/find-films.spec.ts
+++ src/domain/usecases/find-films.spec.ts
@@ -3,9 +3,19 @@ import { FindFilmsUsecase } from './find-films'
 import { FindFilmsRepository } from '../contracts/repositories/film-repository'
 
 describe('FindFilms Usecase', () => {
+  const mockFilms = () => ([
+    {
+      title: 'any_title',
+      originalTitle: 'any_title',
+      description: 'any_description',
+      releaseDate: 'any_description',
+      score: 'any_score'
+    }
+  ])
+
   class FindFilmsRepositoryStub implements FindFilmsRepository {
-    async find(input: FindFilmsRepository.Input): Promise<{ title: string; orginalTitle: string; description: string; releaseDate: string; score: string }[]> {
-      return []
+    async find(input: FindFilmsRepository.Input): Promise<{ title: string; originalTitle: string; description: string; releaseDate: string; score: string }[]> {
+      return mockFilms()
     }
   }
   test('should call FindFilmsRepository with correct params', async () => {
@@ -21,5 +31,18 @@ describe('FindFilms Usecase', () => {
     await sut.find(params)
 
     expect(findSpy).toHaveBeenCalledWith(params)
+  })
+
+  test('should return all films on success', async () => {
+    const findFilmsRepositoryStub = new FindFilmsRepositoryStub()
+    const sut = new FindFilmsUsecase(findFilmsRepositoryStub)
+    const params: FindFilms.Input = {
+      limit: 10,
+      offset: 1
+    }
+
+    const expected = await sut.find(params)
+    const result = mockFilms()
+    expect(result).toEqual(expected)
   })
 })
