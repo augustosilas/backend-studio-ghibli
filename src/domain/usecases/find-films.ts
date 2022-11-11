6f8043e0c12f744aaae9a20fb72a680fd6b6c2ff
Silas Augusto <augusto.fernandes775@gmail.com>
2022-11-11 10:52:02 -0300

    feat: ensure FindFilmsUsecase call FindFilmsRepository with correct params
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

---
+++ src/domain/usecases/find-films.ts
@@ -0,0 +1,14 @@
+import { FindFilmsRepository } from "domain/contracts/repositories/film-repository";
+import { FindFilms } from "../contracts/usecases/find-films";
+
+export class FindFilmsUsecase implements FindFilms {
+
+  constructor(
+    private readonly findFilmsRepository: FindFilmsRepository
+  ) { }
+
+  async find(input: FindFilms.Input): Promise<FindFilms.Result> {
+    await this.findFilmsRepository.find(input)
+    return
+  }
+}
