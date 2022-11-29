a52f15e596dbbfbea63211625fceecce2017bfc6
Silas Augusto <augusto.fernandes775@gmail.com>
2022-11-29 16:00:00 -0300

    feat: ensure SaveFilmsUsecase call AddFilmsRepository with correct inputs
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- src/domain/usecases/save-films.ts
+++ src/domain/usecases/save-films.ts
@@ -1,12 +1,15 @@
+import { AddFilmsRepository } from "../../domain/contracts/repositories/film-repository";
 import { FindFilmsGateway } from "../../domain/contracts/gateways/studio-ghibli-gateway";
 import { SaveFilms } from "../../domain/contracts/usecases/save-films";
 
 export class SaveFilmsUsecase implements SaveFilms {
   constructor(
-    private readonly findFilmsGateway: FindFilmsGateway
+    private readonly findFilmsGateway: FindFilmsGateway,
+    private readonly addFilmsRepository: AddFilmsRepository
   ) { }
 
   async save(): Promise<void> {
-    await this.findFilmsGateway.find({ limit: 30 })
+    const films = await this.findFilmsGateway.find({ limit: 30 })
+    await this.addFilmsRepository.add(films)
   }
 }
