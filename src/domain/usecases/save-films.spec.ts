11a259e62b93b2b6b3f66b03ff1f78c8afe0fa56
Silas Augusto <augusto.fernandes775@gmail.com>
2022-11-29 15:00:06 -0300

    test: ensure SaveFilmsUsecase throw if FindFilmsGateway throws
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

--- src/domain/usecases/save-films.spec.ts
+++ src/domain/usecases/save-films.spec.ts
@@ -2,13 +2,13 @@ import { FindFilmsGateway } from '../contracts/gateways/studio-ghibli-gateway'
 import { SaveFilmsUsecase } from './save-films'
 
 describe('SaveFilms Usecase', () => {
-  test('should call FindFilmsGateway with correct input', async () => {
-    class FindFilmsGatewayStub implements FindFilmsGateway {
-      async find(input?: FindFilmsGateway.Input): Promise<FindFilmsGateway.Film[]> {
-        return Promise.resolve([])
-      }
+  class FindFilmsGatewayStub implements FindFilmsGateway {
+    async find(input?: FindFilmsGateway.Input): Promise<FindFilmsGateway.Film[]> {
+      return Promise.resolve([])
     }
+  }
 
+  test('should call FindFilmsGateway with correct input', async () => {
     const findFilmsGatewayStub = new FindFilmsGatewayStub()
     const sut = new SaveFilmsUsecase(findFilmsGatewayStub)
 
@@ -18,5 +18,16 @@ describe('SaveFilms Usecase', () => {
     await sut.save()
 
     expect(findSpy).toHaveBeenCalledWith(result)
+  })
+
+  test('should throw if FindFilmsGateway throws', async () => {
+    const findFilmsGatewayStub = new FindFilmsGatewayStub()
+    const sut = new SaveFilmsUsecase(findFilmsGatewayStub)
+
+    jest.spyOn(findFilmsGatewayStub, 'find').mockImplementationOnce(() => { throw new Error() })
+
+    const result = sut.save()
+
+    expect(result).rejects.toThrow(new Error())
   })
 })
