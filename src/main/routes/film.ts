6701ef9e89fe150f1a845c5bff3a46a8e015f23e
Silas Augusto <augusto.fernandes775@gmail.com>
2022-11-29 18:25:44 -0300

    feat: release movie routes
    Signed-off-by: Silas Augusto <augusto.fernandes775@gmail.com>

---
+++ src/main/routes/film.ts
@@ -0,0 +1,10 @@
+import { adaptExpressRoute as adapt } from "../../main/adapters/express-router"
+
+import { Router } from 'express'
+import { makeFindFilmsController } from "../../main/factories/application/controllers/find-films-controller"
+import { makeSaveFilmsController } from "../../main/factories/application/controllers/save-films-controller"
+
+export default (router: Router): void => {
+  router.get('/films', adapt(makeFindFilmsController()))
+  router.post('/films', adapt(makeSaveFilmsController()))
+}
